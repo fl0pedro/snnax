d019b0e621033b5539e49801778dcdec31a18b18
Emre Neftci <e.neftci@fz-juelich.de>
2024-04-26 12:22:16 +0000

    fixed srm

--- src/snnax/snn/layers/srm.py
+++ src/snnax/snn/layers/srm.py
@@ -7,7 +7,7 @@ import jax.random as jrand
 
 from equinox import static_field
 import equinox as eqx
-from .stateful import StatefulLayer
+from .stateful import StatefulLayer, TrainableArray
 from ...functional.surrogate import superspike_surrogate
 
 
@@ -18,21 +18,23 @@ class SRM(StatefulLayer):
     TODO
     """
     layer: eqx.Module 
-    decay_constants: Union[Sequence[float], jnp.ndarray] = static_field()
+    decay_constants: Union[Sequence[float], jnp.ndarray, TrainableArray]     
     threshold: Union[float, jnp.ndarray] = static_field()
     spike_fn: Callable = static_field()
-    reset_val: Optional[Union[float, jnp.ndarray]] = static_field()
+    reset_val: Optional[Union[float, jnp.ndarray, TrainableArray]]
     stop_reset_grad: bool = static_field()
 
     def __init__(self, 
                 layer: eqx.Module,
-                decay_constants: Union[Sequence[float], jnp.ndarray],
+                decay_constants: Union[Sequence[float], jnp.ndarray, TrainableArray],
                 *args,
                 spike_fn: Callable = superspike_surrogate(10.),
                 threshold: Union[float, jnp.ndarray] = 1.,
-                reset_val: Optional[Union[float, jnp.ndarray]] = None,
+                reset_val: Optional[Union[float, jnp.ndarray, TrainableArray]] = None,
                 stop_reset_grad: Optional[bool] = True,
                 init_fn: Optional[Callable] = None,
+                shape: Union[Sequence[int],int,None] = None,
+                key: jrand.PRNGKey = jax.random.PRNGKey(0),
                 **kwargs) -> None:
         """**Arguments**:
 
@@ -40,6 +42,7 @@ class SRM(StatefulLayer):
         - `decay_constants`: Decay constants for the leaky integrate-and-fire neuron.
             Index 0 describes the decay constant of the membrane potential,
             Index 1 describes the decay constant of the synaptic current.
+            Index 2 describes the decay constant of the refractory period.
         - `spike_fn`: Spike treshold function with custom surrogate gradient.
         - `threshold`: Spike threshold for membrane potential. Defaults to 1.
         - `reset_val`: Reset value after a spike has been emitted. Defaults to None.
@@ -54,9 +57,19 @@ class SRM(StatefulLayer):
         self.decay_constants = decay_constants
         self.threshold = threshold
         self.spike_fn = spike_fn
-        self.reset_val = reset_val
         self.stop_reset_grad = stop_reset_grad
         self.layer = layer
+
+        if shape is None:
+            self.decay_constants = TrainableArray(decay_constants)
+            self.reset_val = TrainableArray(reset_val)
+        else:
+            _arr = jax.random.uniform(minval=.5,maxval=1.0, shape=shape, key=key)
+            self.decay_constants = TrainableArray(_arr) 
+            _arr = jax.random.uniform(minval=0.,maxval=2.0, shape=shape, key=key)
+            self.reset_val = TrainableArray(_arr) 
+
+
 
     def init_state(self, 
                    shape: Union[Sequence[int], int], 
@@ -66,10 +79,11 @@ class SRM(StatefulLayer):
         init_state_P = jnp.zeros(shape)
         init_state_Q = jnp.zeros(shape) # The synaptic currents are initialized as zeros
         print(init_state_Q.shape)
-        mock_input = self.layer(init_state_Q)
+        output = self.layer(init_state_Q)
         
-        init_state_S = jnp.zeros(mock_input.shape) # The synaptic currents are initialized as zeros
-        return [init_state_P, init_state_Q, init_state_S]
+        init_state_R = jnp.zeros(output.shape) # The synaptic currents are initialized as zeros
+        init_state_S = jnp.zeros(output.shape) # The synaptic currents are initialized as zeros
+        return [init_state_P, init_state_Q, init_state_R, init_state_S]
     
     def init_out(self, 
                 shape: Union[int, Sequence[int]], 
@@ -77,33 +91,27 @@ class SRM(StatefulLayer):
         # The initial ouput of the layer. Initialize as an array of zeros.
         return self.layer(jnp.zeros(shape))
    
-
-
     def __call__(self, 
                 state: Sequence[jnp.ndarray], 
                 synaptic_input: jnp.ndarray,
                 *, key: Optional[jrand.PRNGKey] = None) -> Sequence[jnp.ndarray]:
 
-        P, Q = state[0], state[1]
+        P, Q, R, S = state
 
-        alpha = self.decay_constants[0]
-        beta = self.decay_constants[1]
-        
+        alpha = jax.lax.clamp(0.5, self.decay_constants.data[0], 1.0)
+        beta = jax.lax.clamp(0.5, self.decay_constants.data[1], 1.0)
+        gamma = jax.lax.clamp(0.5, self.decay_constants.data[2], 1.0)
+        reset_val = jax.lax.clamp(0.0, self.reset_val.data, 2.0)
+
         P = alpha*P + (1.-alpha)*synaptic_input
-        Q = beta*Q + P
-        membrane_potential = self.layer(Q)
+        Q = beta*Q + (1.-beta)*P
+        R = gamma*R + reset_val*lax.stop_gradient(S)
+        membrane_potential = self.layer(Q)-reset_val*R
         spike_output = self.spike_fn(membrane_potential - self.threshold)
         
-        # if self.reset_val is None:
-        #     reset_pot = membrane_potential*spike_output
-        # else:
-        #     reset_pot = self.reset_val*spike_output
 
-        # optionally stop gradient propagation through refectory potential       
-        # refectory_potential = lax.stop_gradient(reset_pot) if self.stop_reset_grad else reset_pot
-        # membrane_potential = membrane_potential - refectory_potential
 
-        state = [P, Q, spike_output]
+        state = [P, Q, R, spike_output]
         return state, spike_output
 
 
